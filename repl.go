cc4c205be7a34c20d19ccd51fda51895cfd6b291
Jason Martinez <jason@bigpictureranch.com>
2025-08-03 12:18:50 -0700

    can fetch locations, with Next and Previous

--- repl.go
+++ repl.go
@@ -5,45 +5,53 @@ import (
 	"fmt"
 	"os"
 	"strings"
+
+	"github.com/jmartaudio/pokedexcli/internal/pokeapi"
 )
 
-func startRepl() {
-        scanner := bufio.NewScanner(os.Stdin)
+type config struct {
+	pokeapiClient    pokeapi.Client
+	nextLocationsURL *string
+	prevLocationsURL *string
+}
 
-        for {
-                fmt.Print("Pokedex > ")
-                scanner.Scan()
+func startRepl(cfg *config) {
+	reader := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		reader.Scan()
 
-		input := cleanInput(scanner.Text())
-                if len(input) == 0 {
-                        fmt.Println("Enter some text")
-                        continue
-                }
-		userCmd := input[0]
+		words := cleanInput(reader.Text())
+		if len(words) == 0 {
+			continue
+		}
 
-                cmd, exists := getCommands()[userCmd]
-                if exists {
-			err := cmd.callback() 
+		commandName := words[0]
+
+		command, exists := getCommands()[commandName]
+		if exists {
+			err := command.callback(cfg)
 			if err != nil {
 				fmt.Println(err)
 			}
 			continue
-                } else {
-                        fmt.Printf("Command: %s Not Found\n", userCmd)
+		} else {
+			fmt.Println("Unknown command")
 			continue
-                }
-        }
+		}
+	}
 }
 
 func cleanInput(text string) []string {
-        inputSlice := strings.Fields(strings.ToLower(text))
-        return inputSlice
+	output := strings.ToLower(text)
+	words := strings.Fields(output)
+	return words
 }
 
 type cliCommand struct {
-        name        string
-        description string
-        callback    func() error
+	name        string
+	description string
+	callback    func(*config) error
 }
 
 func getCommands() map[string]cliCommand {
@@ -52,6 +60,16 @@ func getCommands() map[string]cliCommand {
 			name:        "help",
 			description: "Displays a help message",
 			callback:    commandHelp,
+		},
+		"map": {
+			name:        "map",
+			description: "Get the next page of locations",
+			callback:    commandMapf,
+		},
+		"mapb": {
+			name:        "mapb",
+			description: "Get the previous page of locations",
+			callback:    commandMapb,
 		},
 		"exit": {
 			name:        "exit",
