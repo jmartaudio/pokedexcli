a5d2749a2b89cd1624d97509920c90b92ee86b3b
Jason Martinez <jason@bigpictureranch.com>
2025-08-05 10:02:05 -0700

    working cache

--- internal/pokeapi/client.go
+++ internal/pokeapi/client.go
@@ -3,18 +3,22 @@ package pokeapi
 import (
 	"net/http"
 	"time"
+
+	"github.com/jmartaudio/pokedexcli/internal/pokecache"
 )
 
 // Client -
 type Client struct {
 	httpClient http.Client
+	cache      *pokecache.Cache
 }
 
 // NewClient -
-func NewClient(timeout time.Duration) Client {
+func NewClient(timeout time.Duration, interval time.Duration) Client {
 	return Client{
 		httpClient: http.Client{
 			Timeout: timeout,
 		},
+		cache: pokecache.NewCache(interval),
 	}
 }
--- internal/pokeapi/location_list.go
+++ internal/pokeapi/location_list.go
@@ -4,46 +4,48 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
-	"time"
-
-	"github.com/jmartaudio/pokedexcli/internal/pokecache"
 )
 
 // ListLocations -
-func (c *Client) ListLocations(pageURL *string, interval time.Duration) (RespShallowLocations, error) {
-	cache := pokecache.NewCache(interval)
-
+func (c *Client) ListLocations(pageURL *string) (RespShallowLocations, error) {
 	url := baseURL + "/location-area"
 	if pageURL != nil {
 		url = *pageURL
 	}
 
-	val, ok := cache.Get(*pageURL)
+	dat, ok := c.cache.Get(url)
 	if !ok {
+		req, err := http.NewRequest("GET", url, nil)
+		if err != nil {
+			return RespShallowLocations{}, err
+		}
+		resp, err := c.httpClient.Do(req)
+		if err != nil {
+			return RespShallowLocations{}, err
+		}
+		defer resp.Body.Close()
+		
+		dat, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return RespShallowLocations{}, err
+		}
 
-	}
+		c.cache.Add(url, dat)
 
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return RespShallowLocations{}, err
-	}
+		locationsResp := RespShallowLocations{}
+		err = json.Unmarshal(dat, &locationsResp)
+		if err != nil {
+			return RespShallowLocations{}, err
+		}
 
-	resp, err := c.httpClient.Do(req)
-	if err != nil {
-		return RespShallowLocations{}, err
-	}
-	defer resp.Body.Close()
-
-	dat, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return RespShallowLocations{}, err
-	}
-
-	locationsResp := RespShallowLocations{}
-	err = json.Unmarshal(dat, &locationsResp)
-	if err != nil {
-		return RespShallowLocations{}, err
+		return locationsResp, nil
+	} else {
+		locationsResp := RespShallowLocations{}
+		err := json.Unmarshal(dat, &locationsResp)
+		if err != nil {
+			return RespShallowLocations{}, err
 	}
 
 	return locationsResp, nil
+	}
 }
